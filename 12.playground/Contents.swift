d329c8328d400101c65d24c0cf7f7c39dcbdd462
jeon yul <jeonyul.citylabs@gmail.com>
2023-08-29 12:38:36 +0900

    update chapter 12

---
+++ 12.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+// dictionary : key value : 순서 x
+
+let names = Dictionary<String, String>()
+let names2 = [String : String]()
+let names3: [String : String] = [:]
+let names4: Dictionary<String, String> = [:]
+
+let names5 = ["cityA" : "Seoul", "CityB" : "LA"]
