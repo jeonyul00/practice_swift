288e295e6fa4eb84a976042b9ac25284941b0c77
jeonyul <jeonyul00@gmail.com>
2025-01-01 15:54:12 +0900

    update

---
+++ 2025/01.변수와상수.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// 변수 상수
+
+// 변수
+var score: Int = 0
+score
+score = 1
+
+// 상수
+let name:String = "jeon"
+// name = "" x -> 한 번 메모리에 올라가면 재할당 x
+
+// 선언과 할당을 따로 해도 됨 -> 선언이 아니고 할당 시 메모리에 올라감
+let name2:String
+name2 = "yul"
+// name2 = "yul" x -> 메모리에 올라갔으니 재할당 x
